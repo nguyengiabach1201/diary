f49cccdd96da41901921f57320ffa8d0a40dd66e
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-10-03 22:24:57 +0700

    Update diary.js

--- src/diary.js
+++ src/diary.js
@@ -1,11 +1,2 @@
-// add(form1('12th January 2024!', 'Happy birthday to me! Today is really a fun and a really joyful day not only for me but also for my family too!', './imgs/jan12.jpg', 'jan12'));
-// add(img('./imgs/bg/cloud.jpg'));
-// add(form2('13th January 2024!', 'How fun this day is!', './imgs/jan12.jpg', 'jan13'));
+add(form1('3/10/2024', 'Đã add anh Chương lên nguyengiabach.com. Hôm nay học bồi dưỡng vui lắm, giải hình mà thầy Bảo khen hoài, Anh thì cô Thu cho làm đề :)) Hôm nay lớp chuyển sang bên dãy E để sơn, cô Hằng đi nhầm lớp nên ì ạch đi bộ qua dãy E nhìn hài deso. Được thêm tận 2 điểm tốt Toán.', './imgs/2024-10-3.jpg', '2024-10-3'));
 
-
-// // add(form2('12th January!', 'test', './imgs/test.jpg', 'jan15'));
-// // add(img('./imgs/bg/balloon2.png','right'));
-
-// setTimeout(function () {
-//     connect('jan12', 'jan13');
-// }, 10);
